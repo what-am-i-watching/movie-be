75f74da779a64b6da6625e41b55b34e22c198a61
Sheryl Stillman <sheryl.stillman@gmail.com>
2026-01-15 12:24:42 -0700

    Update add user_movie to edit or create new one, add destroy user_movie

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -8,13 +8,25 @@ class UserMoviesController < ApplicationController
   end
 
   def create
-    @user_movie = current_user.user_movies.find_or_create_by(user_movie_params)
+    if
+      @user_movie = current_user.user_movies.find_by({movie_id: user_movie_params["movie_id"], user_id: current_user["id"]})
+      @user_movie.update(user_movie_params)
+      # NOTE: since we show by create date, the user watched order is off
+    else
+      @user_movie = current_user.user_movies.find_or_create_by(user_movie_params)
+    end
+
 
     if @user_movie.save
       render json: @user_movie, status: :created, location: @user_movie
     else
       render json: @user_movie.errors, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    user_movie = UserMovie.find(params['id'])
+    user_movie.destroy
   end
 
   private
