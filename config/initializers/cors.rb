6d56e4848e9082c40dd9f114b4753cd5d568bf2c
Sheryl Stillman <sheryl.stillman@gmail.com>
2026-01-21 14:11:18 -0700

    Update cors to accept new domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173", "https://movie-fe.pages.dev"
+    origins "http://localhost:5173", "https://movie-fe.pages.dev, whatamiwatching.info, www.whatamiwatching.info"
 
     resource "*",
       headers: :any,
