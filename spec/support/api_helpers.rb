ddc985cb9beb501cf4bec4ffd4f9ba8e6788606e
Sheryl Stillman <sheryl.stillman@gmail.com>
2026-01-25 13:44:09 -0700

    Add helpers to create data and for expected API responses

--- spec/support/api_helpers.rb
+++ spec/support/api_helpers.rb
@@ -26,6 +26,55 @@ module ApiHelpers
   def authenticated_headers(user)
     auth_headers(user)
   end
+
+  def create_test_movie(attributes = {})
+    Movie.create!({
+      title: 'Test Movie',
+      tmdb_id: 12345,
+      release_year: 2020,
+      poster_url: 'https://example.com/poster.jpg',
+      is_movie: true
+    }.merge(attributes))
+  end
+
+  def create_test_user_movie(user:, movie: nil, **attributes)
+    movie ||= create_test_movie
+    UserMovie.create!({
+      user: user,
+      movie: movie,
+      status: :to_watch
+    }.merge(attributes))
+  end
+
+  # Schema helpers for OpenAPI documentation
+  def error_schema
+    {
+      type: :object,
+      properties: {
+        error: {
+          type: :string,
+          description: 'Error message'
+        },
+        details: {
+          type: :string,
+          description: 'Additional error details (optional)'
+        }
+      },
+      required: [ 'error' ]
+    }
+  end
+
+  def validation_error_schema
+    {
+      type: :object,
+      properties: {
+        errors: {
+          type: :object,
+          description: 'Validation errors by field'
+        }
+      }
+    }
+  end
 end
 
 RSpec.configure do |config|
