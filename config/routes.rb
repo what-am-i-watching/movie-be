cd90a08c71d3ef23612e9ab2ae7b68207828323d
Sheryl Stillman <sheryl.stillman@gmail.com>
2026-01-25 12:56:59 -0700

    Add rswag gem and generate initial structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => "/api-docs"
+  mount Rswag::Api::Engine => "/api-docs"
   resources :movies, only: [ :index, :show, :create ] do
     collection do
       get :search
